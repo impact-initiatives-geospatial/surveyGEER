93382079b1e13b7ba90416d9f8e18143d801625b
zackarno <zackarno@gmail.com>
2022-07-18 11:14:53 +0300

    fetch helper added

---
+++ R/fetch_msna.R
@@ -0,0 +1,24 @@
+#' fetch_msna - helper function to quickly pull msna coordinate files
+#'
+#' @param country_code \code{character} three letter country code (lower-case)
+#'
+#' @return sf data.frame containing msna records
+#' @export
+#'
+#' @examples \dontrun{
+#' library(surveyGEER)
+#' df <-  fetch_msna("com")
+#' }
+
+fetch_msna <- function(country_code="com"){
+  msna_path<- "data/msna/"
+  msna_files <- list.files(msna_path)
+  file_rgx<- glue::glue("_{country_code}\\.rds$")
+  # stringr::str_detect(string = msna_files,pattern = file_rgx)
+  foi_name<- stringr::str_subset(string = msna_files,pattern = file_rgx)
+  foi<- glue::glue("{msna_path}{foi_name}")
+  readr::read_rds(foi) |>
+    dplyr::mutate(
+      country_code=country_code
+    )
+}

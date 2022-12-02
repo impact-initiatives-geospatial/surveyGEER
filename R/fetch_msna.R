3870d6e197199e4e6f67423ab5ecb36d04e0c432
zackarno <zackarno@gmail.com>
2022-12-02 10:15:52 -0500

    add flexibility for targets not to be reinitiated

--- R/fetch_msna.R
+++ R/fetch_msna.R
@@ -22,6 +22,27 @@ fetch_msna <- function(country_code="col"){
       country_code=country_code
     )
 }
+#' fetch_msna_path - helper function to quickly pull msna coordinate file path
+#'
+#' @param country_code \code{character} three letter country code (lower-case)
+#' @description return msna data based on suplied country code
+#' @return sf data.frame containing msna records
+#' @export
+#'
+#' @examples \dontrun{
+#' library(surveyGEER)
+#' df <-  fetch_msna("com")
+#' }
+
+fetch_msna_path <- function(country_code="col"){
+  msna_path<- here::here("data/msna")
+  msna_files <- list.files(msna_path)
+  file_rgx<- glue::glue("_{country_code}\\.rds$")
+  # stringr::str_detect(string = msna_files,pattern = file_rgx)
+  foi_name<- stringr::str_subset(string = msna_files,pattern = file_rgx)
+  foi<- glue::glue("{msna_path}/{foi_name}")
+  return(foi)
+}
 
 #' Title
 #'

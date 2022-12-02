d4b14ed4590eae70976dfc76e5fd5e42485e2a10
zackarno <zackarno@gmail.com>
2022-12-02 10:14:51 -0500

    add flexibility for targets not to be reinitiated

--- R/is_checks.R
+++ R/is_checks.R
@@ -1,8 +1,17 @@
 
 #' check if geo data has already been set up
 
-is_geo_data_set <-  function(){
-  file.exists(here::here("vault/lookup.rds"))| file.exists(here::here("data_share/coords_anonymized.rds.rds"))
+is_geo_data_set <-  function(country_code=NULL){
+  if(is.null(country_code)){
+    cond <- file.exists(here::here("vault/lookup.rds"))| file.exists(here::here("data_share/coords_anonymized.rds"))
+  }
+  if(!is.null(country_code)){
+    lookup_file <- paste0("vault/",country_code,"_lookup.rds")
+    coord_anon_file <- paste0("vault/",country_code,"_coords_anonymized.rds")
+    cond <- file.exists(here::here(lookup_file))| file.exists(here::here(coord_anon_file))
+  }
+  return(cond)
+
 
 }
 

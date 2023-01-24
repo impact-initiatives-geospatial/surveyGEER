f373e7cf1c128ed30acc40b9af5fc674052f5254
zackarno <zackarno@gmail.com>
2023-01-24 12:36:37 +0300

    seems band name in GEE catalogue changed to include "_1" at the end

--- R/extract_growing_season_length_viirs.R
+++ R/extract_growing_season_length_viirs.R
@@ -16,7 +16,7 @@ extract_growing_season_length_viirs <-  function(geom_sf, yoi=2013:2020,scale=50
   tidy_ic <-  as_tidyee(ic)
 
   tidy_ic_filtered <- tidy_ic |>
-    select("Growing_Season_Length") |>
+    select("Growing_Season_Length_1") |>
     filter(year %in% yoi)
   res <- tidy_ic_filtered |>
     ee_extract_tidy(y = geom_sf,stat = "median",via = "drive",sf = F,scale = scale)
